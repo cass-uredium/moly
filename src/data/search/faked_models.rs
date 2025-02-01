ccf5cb7fc597334515e11a820abfc7adc35d7c2a
cassaundra <cass@uredium.dev>
2025-01-31 16:01:37 -0800

    chore: upgrade rand to 0.9.0

--- src/data/search/faked_models.rs
+++ src/data/search/faked_models.rs
@@ -1,15 +1,15 @@
 use moly_protocol::data::{File, Model};
 
 use lipsum::lipsum;
-use rand::distributions::{Alphanumeric, DistString};
+use rand::distr::{Alphanumeric, SampleString};
 use rand::Rng;
 
 fn random_string(size: usize) -> String {
-    Alphanumeric.sample_string(&mut rand::thread_rng(), size)
+    Alphanumeric.sample_string(&mut rand::rng(), size)
 }
 
 pub fn get_faked_models(models: &Vec<Model>) -> Vec<Model> {
-    let mut rng = rand::thread_rng();
+    let mut rng = rand::rng();
 
     models
         .iter()
@@ -25,20 +25,20 @@ pub fn get_faked_models(models: &Vec<Model>) -> Vec<Model> {
                 new_model.name = format!(
                     "{}-{}-{}{}-{}-{}",
                     lipsum(1),
-                    rng.gen_range(0..10),
+                    rng.random_range(0..10),
                     random_string(1).to_uppercase(),
-                    rng.gen_range(0..10),
+                    rng.random_range(0..10),
                     lipsum(1),
                     lipsum(1),
                 );
             }
 
             if model.size.is_empty() {
-                new_model.size = format!("{}B", rng.gen_range(1..10));
+                new_model.size = format!("{}B", rng.random_range(1..10));
             };
 
             if model.requires.is_empty() {
-                new_model.requires = match rng.gen_range(0..3) {
+                new_model.requires = match rng.random_range(0..3) {
                     0 => "4GB+ RAM".to_string(),
                     1 => "8GB+ RAM".to_string(),
                     2 => "16GB+ RAM".to_string(),
@@ -47,7 +47,7 @@ pub fn get_faked_models(models: &Vec<Model>) -> Vec<Model> {
             }
 
             if model.architecture.is_empty() {
-                new_model.architecture = match rng.gen_range(0..3) {
+                new_model.architecture = match rng.random_range(0..3) {
                     0 => "Mistral".to_string(),
                     1 => "StableLM".to_string(),
                     2 => "LlaMa".to_string(),
@@ -56,11 +56,11 @@ pub fn get_faked_models(models: &Vec<Model>) -> Vec<Model> {
             }
 
             if model.like_count == 0 {
-                new_model.like_count = rng.gen_range(1..1000);
+                new_model.like_count = rng.random_range(1..1000);
             };
 
             if model.download_count == 0 {
-                new_model.download_count = rng.gen_range(0..10000);
+                new_model.download_count = rng.random_range(0..10000);
             };
 
             // filling files
@@ -73,7 +73,7 @@ pub fn get_faked_models(models: &Vec<Model>) -> Vec<Model> {
                     if new_file.quantization.is_empty() {
                         new_file.quantization = format!(
                             "Q{}_{}_{}",
-                            rng.gen_range(0..10),
+                            rng.random_range(0..10),
                             random_string(1).to_uppercase(),
                             random_string(1).to_uppercase()
                         );
@@ -84,19 +84,19 @@ pub fn get_faked_models(models: &Vec<Model>) -> Vec<Model> {
                         new_file.name = format!(
                             "{}-{}-{}-{}-{}.{}.gguf",
                             lipsum(1),
-                            rng.gen_range(0..10),
+                            rng.random_range(0..10),
                             random_string(5),
                             lipsum(1),
                             new_file.quantization,
-                            rng.gen_range(0..10),
+                            rng.random_range(0..10),
                         );
                     }
 
                     if file.size.is_empty() {
-                        new_file.size = rng.gen_range(100000000..999999999).to_string();
+                        new_file.size = rng.random_range(100000000..999999999).to_string();
                     };
 
-                    new_file.featured = rng.gen_bool(0.15);
+                    new_file.featured = rng.random_bool(0.15);
 
                     new_file
                 })
