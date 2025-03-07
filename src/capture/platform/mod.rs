3dcbdf0f16d1ac4549793dccfbda2d75c7465c75
cassaundra <cass@uredium.dev>
2025-03-07 13:22:10 -0800

    capture: implement macOS service/provider
    - Introduce "Capture in Moly" service in `Info.plist`.
    - Implement capture service provider for macOS.

--- src/capture/platform/mod.rs
+++ src/capture/platform/mod.rs
@@ -1,2 +1,9 @@
+#[cfg(target_vendor = "apple")]
+mod apple;
+#[cfg(target_vendor = "apple")]
+pub(super) use apple::*;
+
+#[cfg(not(target_vendor = "apple"))]
 mod unsupported;
+#[cfg(not(target_vendor = "apple"))]
 pub(super) use unsupported::*;
